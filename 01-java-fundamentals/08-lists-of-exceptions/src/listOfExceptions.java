4ec9e5a82da5ffb95201ca129f4a64af17b47083
Michael Smith <Michael.t.smith95@gmail.com>
2021-03-14 11:53:34 -0400

    assed List of Exceptions assignment

---
+++ 01-java-fundamentals/08-lists-of-exceptions/src/listOfExceptions.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class listOfExceptions {
+	public static void main(String[] args) {
+		ArrayList<Object> myList = new ArrayList<Object>();
+		myList.add("13");
+		myList.add("Hello world");
+		myList.add(48);
+		myList.add("Goodbye World");
+
+		for (int i = 0; i < myList.size(); i++) {
+			try {
+			Integer castedValue = (Integer) myList.get(i);
+			} catch (ClassCastException e) { 
+				System.out.println("This didnt work");
+				System.out.println("Location of breakage: " + myList.indexOf(i));
+				System.out.println("Value that broken index: " + myList.get(i) + "\n");
+				
+			}
+		}
+	}
+}
