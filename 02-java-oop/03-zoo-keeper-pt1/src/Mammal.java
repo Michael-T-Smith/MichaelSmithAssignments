936098431cf76a4ca11c421eff392f669333eb3a
Michael Smith <Michael.t.smith95@gmail.com>
2021-03-17 13:02:59 -0400

    Zoo Keeper Part 1 Assignment added

---
+++ 02-java-oop/03-zoo-keeper-pt1/src/Mammal.java
@@ -0,0 +1,11 @@
+
+public class Mammal {
+	public int energyLevel = 100;
+	
+	public int displayEnergy() {
+		System.out.println("The animals energy level is " + this.energyLevel);
+		return this.energyLevel;
+	}
+	
+	
+}
